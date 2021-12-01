5eb4295970102bbae0e3ec566a6c2a52444e67d8
Sturgen <dodi.alexsander@gmail.com>
2021-12-02 06:26:54 +0700

    adding video1

--- src/components/showreel/Card1/index.js
+++ src/components/showreel/Card1/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Card, Row, Col, Container } from 'react-bootstrap';
-const Card1 = () => {
+const Card1 = ({ src }) => {
     return (
         <div>
             <Container fluid>
                 <Row style={row}>
                     <Col>
                         <Card style={card}>
-                            <iframe width="100%" height="100%" src="https://www.youtube.com/embed/tXO9cp-A8aM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                            <iframe width="100%" height="100%" src={src} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                             {/* <Card.Body>
                                 <Card.Img />
                                 <Card.ImgOverlay>
