e64985ef48a13aae4f9e8dbbfcd65d3fc81dc4ed
Sturgen <dodi.alexsander@gmail.com>
2021-12-01 07:59:05 +0700

    adding video

--- src/components/showreel/Card1/index.js
+++ src/components/showreel/Card1/index.js
@@ -7,13 +7,14 @@ const Card1 = () => {
                 <Row style={row}>
                     <Col>
                         <Card style={card}>
-                            <Card.Body>
+                            <iframe width="100%" height="100%" src="https://www.youtube.com/embed/tXO9cp-A8aM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                            {/* <Card.Body>
                                 <Card.Img />
                                 <Card.ImgOverlay>
                                     <Card.Title></Card.Title>
                                     <Card.Text></Card.Text>
                                 </Card.ImgOverlay>
-                            </Card.Body>
+                            </Card.Body> */}
                         </Card>
                     </Col>
                 </Row>
